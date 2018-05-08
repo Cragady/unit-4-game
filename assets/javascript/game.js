d962f2dc5dbc8fd9bc2bad610a7eb7d8b8412171
Cragady <pinkdotdreamer@gmail.com>
2018-05-07 23:42:03 -0600

    some basic layouts

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//use js/jq to change the css display value
+//for the pictures row
+//during the fighting portion, the pictures should
+//be side by side like in an actual rpg game with
+//the fight log below it updating with a
+//scroll bar that pushes the new information at the
+//bottom with the older logs scrolling out of view
+//at the top
