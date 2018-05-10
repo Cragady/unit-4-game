c90bce877a3845f99997fc2b804d313692ffad6d
Cragady <pinkdotdreamer@gmail.com>
2018-05-09 23:31:01 -0600

    fixed data, added button functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -51,38 +51,75 @@ and after. Or use a for loop for appending/prepending*/
 
 //the above is a working example of how to move around
 //clicked pictures 
-var newDiv = $("<div>");
+var newHead = $("<h2>");
 var readyChar = $("#pictures-row");
 var sfc = ["pic1", "pic2", "pic3", "pic4"];
+var charHealth = "";
+var charStats = "";
 var chosenChar = false;
 var chosenEnem = false;
 var enemSet = false;
+var gameOver = true;
 // var movingPictures = $("#pictures-row").detach();
 
 $(document).ready(function(){
-    readyChar.append(newDiv.text("Choose your Character!"));
+    readyChar.append(newHead.text("Choose your Character!"));
+    newHead.attr("class", "col-12 text-info");
     $("div").click(function(){
         var selected = $(this).attr("id");
         if ((sfc.includes(selected))){
             if (chosenChar === false){
                 $(this).detach();
                 $("#player-char").append(this);
-                newDiv.empty();
-                readyChar.append(newDiv.text("Choose your enemy!"));
+                newHead.empty();
+                newHead.attr("class", "col-12 text-danger");
+                readyChar.append(newHead.text("Choose your enemy!"));
+                charStats = $(this).data('stats');
+                charHealth = charStats[0];
                 chosenChar = true;
             } else if ((chosenChar === true) && (chosenEnem === false)){
                 $(this).detach();
                 $("#opponent").append(this);
-                newDiv.empty();
+                newHead.empty();
                 $("#pictures-row").css("display", "none");
                 chosenEnem = true;
-                //code here to extract damage, healt, etc.
+                //code here to extract damage, health, etc.
                 enemSet = true;
             }
-        }
-        // if (enemSet === true){
-        //     $("#pictures-row").css("display", "flex");
-        //     this is for later.
-        // }
-    })
+        };
+
+        if (enemSet === true){
+            $("#atk-btn").css("display", "flex");
+            $("#rst-btn").css("display", "flex");
+        };
+
+        
+        //  if (enemSet === true){
+        //      $("#pictures-row").css("display", "flex");
+        //      //this is for later.
+        //      $.each(sfc, function(i){
+        //          var tar = $("#" + sfc[i]);
+        //          $("#pictures-row").append(tar);
+        //          //and this will be good for reset
+        //          //btw, prolly don't use var "tar"
+        //          console.log(sfc[i]);
+        //      })
+        //  }
+    });
+    $("#atk-btn").click(function(){
+        if (enemSet === true){
+            charHealth--;
+            console.log(charHealth);
+        };
+    });
+    $("#rst-btn").click(function() {
+        if (gameOver === true){
+            $("#pictures-row").css("display", "flex");
+            $.each(sfc, function(i){
+                var resetPicPos = $("#" + sfc[i]);
+                $("#pictures-row").append(resetPicPos);
+                console.log(sfc[i]);
+            });
+        };
+    });
 });
