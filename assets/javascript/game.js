5c34b662ddc6c5de846416c887667f5a9b3a7d67
Cragady <pinkdotdreamer@gmail.com>
2018-05-11 20:04:39 -0600

    Some minor tweaking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,9 @@
-//use js/jq to change the css display value
-//for the pictures row
-//during the fighting portion, the pictures should
-//be side by side like in an actual rpg game with
 //the fight log below it updating with a
 //scroll bar that pushes the new information at the
 //bottom with the older logs scrolling out of view
 //at the top
-
-/* use variables to keep track of scoring, and 
-a switching true/false variable to trigger
-a variable reset in the event of winning/losing
-or active fight */
-
-/* instead of using css, just use the detach and 
-like in the function below make use of prepend append 
-and after. Or use a for loop for appending/prepending*/
-
-// $(document).ready(function(){
-
-
-//     //$pic1 = $("#pic1").detach();
-//     $pic1 = $("#pictures-row").detach();
-//     //$("#rchallengers").append($pic1);
-//     $("#fight-log").append($pic1);
-//     console.log("scripted");
-
-//     $("#pic1").on("click", function(){
-//         $pic1;
-//         $("#pictures-row").append($pic1);
-//         console.log("clicked!");
-//         // sfc = ["#pic1", "#pic2", "#pic3", "#pic4"];
-//         // $("#pictures-row").each(sfc, function(){
-//         //     if ()
-//         // });
-//     })
-
-//     $("div").on("click", function(){
-//         sfc = ["pic1", "pic2", "pic3", "pic4"];
-//         var selected = $(this).attr("id");
-//         if (sfc.includes(selected)){
-//             //$("#pictures-row").prepend(this);
-//             $(this).detach();
-//             $("#rchallengers").append(this);
-//             $("#fight-log").append($("#pictures-row"));
-//             console.log(selected);
-//         };
-//     });
-
-// });
-
-//the above is a working example of how to move around
-//clicked pictures 
+//make sure the reset button comes after winning/losing
+//all fights, 
 var newHead = $("<h2>");
 var readyChar = $("#pictures-row");
 var sfc = ["pic1", "pic2", "pic3", "pic4"];
@@ -65,10 +18,11 @@ var chosenChar = false;
 var chosenEnem = false;
 var enemSet = false;
 var gameOver = true;
+var attackAdd = 0;
 var selected;
 var statted;
 var altThis;
-// var movingPictures = $("#pictures-row").detach();
+
 var gameFunk = {
     chooseChar: function(){
         $(altThis).detach();
@@ -81,9 +35,6 @@ var gameFunk = {
         charAttack = charStats[1];
         chosenChar = true;
         chosenEnem = false;
-        // $("#pictures-row").click(function(){
-        //     chosenEnem = false;
-        // });
         pickedChar.push(selected);
     },
 
@@ -104,48 +55,13 @@ var gameFunk = {
     console.log("premature function run")
     }
 };
-// function chooseChar(){
-//         $(altThis).detach();
-//         $("#player-char").append(altThis);
-//         newHead.empty();
-//         newHead.attr("class", "col-12 text-danger");
-//         readyChar.append(newHead.text("Choose your enemy!"));
-//         charStats = statted;
-//         charHealth = charStats[0];
-//         charAttack = charStats[1];
-//         chosenChar = true;
-//         chosenEnem = undefined;
-//         // $("#pictures-row").click(function(){
-//         //     chosenEnem = false;
-//         // });
-//         pickedChar.push(selected);
-// };
-
-// function chooseEnem(){
-//     $("#pictures-row").css("display", "flex");
-//     if (pickedChar.includes(selected)){
-//         return;
-//     };
-//     $(altThis).detach();
-//     $("#opponent").append(altThis);
-//     newHead.empty();
-//     $("#pictures-row").click(function(){
-//         $("#pictures-row").css("display", "none");
-//         chosenEnem = true;
-//     });
-//     enemStats = statted;
-//     enemHealth = enemStats[0];
-//     enemCounter = enemStats[2];
-//     pickedChar.push(selected);
-// };
 
 $(document).ready(function(){
     readyChar.append(newHead.text("Choose your Character!"));
     newHead.attr("class", "col-12 text-info");
     $("div").children().click(function(){
         selected = $(this).attr("id");
-        if ((sfc.includes(selected))){
-            
+        if ((sfc.includes(selected))){  
             if (chosenChar === false){
                 statted = $(this).data('stats');
                 altThis = this;
@@ -159,18 +75,13 @@ $(document).ready(function(){
 
         if ((chosenEnem === true) && (charHealth > 0)){
             $("#atk-btn").css("display", "flex");
-            $("#rst-btn").css("display", "flex");
         };
-        // if (chosenEnem === false){
-        //     $("#atk-btn").css("display", "none");
-        //     $("#rst-btn").css("display", "none");
-        // }
     });
 
     $("#atk-btn").click(function(){
         if (chosenEnem === true){
             
-            var dmgGiv = Math.floor(Math.random() * charAttack);
+            var dmgGiv = Math.floor(Math.random() * charAttack) + attackAdd;
             var dmgRec = Math.floor(Math.random() * enemCounter);
             charHealth -= dmgRec;
             enemHealth -= dmgGiv;
@@ -180,15 +91,32 @@ $(document).ready(function(){
                 $("#player-char").find(".health-show").text(0);
                 $("#rst-btn").css("display", "flex");
                 $("#atk-btn").css("display", "none");
-            } else if (enemHealth <= 0){
+                if (enemHealth > 0){
+                    $("#buttonInOut").prepend(newHead.text("You lost!"));
+                    newHead.attr("class", "col-12 text-center text-danger");
+                };
+            }; 
+            
+            if (enemHealth <= 0){
                 $("#opponent").find(".health-show").text(0);
                 $("#atk-btn").css("display", "none");
                 chosenEnem = undefined;
-                //charAttack += 15;
+                attackAdd += 12;
                 newHead.attr("class", "col-12 text-danger");
                 readyChar.append(newHead.text("Choose your enemy!"));
                 $("#opponent").children().css("display", "none");
-            };                
+                if (pickedChar.length === 4){
+                    $("#buttonInOut").prepend(newHead.text("You Won!!!"));
+                    newHead.attr("class", "col-12 text-center text-success");
+                    $("#rst-btn").css("display", "flex");
+                }
+            };
+            
+            if ((charHealth < 0) && (enemHealth < 0)){
+                $("#rst-btn").css("display", "flex");
+                $("#buttonInOut").prepend(newHead.text("It's a tie!"));
+                newHead.attr("class", "col-12 text-center text-warning");
+            };
             
         };
     });
@@ -202,7 +130,7 @@ $(document).ready(function(){
             chosenChar = false;
             chosenEnem = false;
             pickedChar = [];
-            //gameOver = false;
+            attackAdd = 0;
             $.each(sfc, function(i){
                 var resetPicPos = $("#" + sfc[i]);
                 resetPicPos.css("display", "flex");
@@ -216,10 +144,14 @@ $(document).ready(function(){
             readyChar.append(newHead.text("Choose your Character!"));
             newHead.attr("class", "col-12 text-info");
             $("#pictures-row").css("display", "flex");
+            $("#rst-btn").css("display", "none");
     });
 
     $("div").click(function(){
         if(chosenEnem === undefined){
+            if ((pickedChar.length === 4) || (charHealth <= 0)){
+                return;
+            };
             $("#pictures-row").css("display", "flex");
             selected = $(this).attr("id");
             console.log(selected);
