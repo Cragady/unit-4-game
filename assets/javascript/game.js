988f369ff22d5017af65783d3af4cc28a4520a7e
Cragady <pinkdotdreamer@gmail.com>
2018-05-12 14:15:47 -0600

    organized into object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,6 +26,36 @@ var statted;
 var altThis;
 
 var gameFunk = {
+
+    gameStart: function(){
+        readyChar.append(newHead.text("Choose your Character!"));
+        newHead.attr("class", "col-12 text-info");
+        $("div").children().click(function(){
+            selected = $(this).attr("id");
+            if ((sfc.includes(selected))){  
+                if (chosenChar === false){
+                    statted = $(this).data('stats');
+                    altThis = this;
+                    gameFunk.chooseChar();
+                } else if ((chosenChar === true) && (chosenEnem === false)){
+                    statted = $(this).data('stats');
+                    altThis = this;
+                    gameFunk.chooseEnem();
+                }
+            };
+
+            if ((chosenEnem === true) && (charHealth > 0)){
+                $("#atk-btn").css("display", "flex");
+            };
+        });
+
+        gameFunk.attackButton();
+
+        gameFunk.resetButton();
+
+        gameFunk.chooseAgainEnem();
+    },
+
     chooseChar: function(){
         $(altThis).detach();
         $("#player-char").append(altThis);
@@ -59,76 +89,56 @@ var gameFunk = {
     battleLog: function(){
         var loggedDmg = $("<div>").text("You deal " + dmgGiv + " damage, and you take " + dmgRec +" damage.");
         $("#fight-log").prepend(loggedDmg, "<hr>");
-    }
-};
+    },
 
-$(document).ready(function(){
-    readyChar.append(newHead.text("Choose your Character!"));
-    newHead.attr("class", "col-12 text-info");
-    $("div").children().click(function(){
-        selected = $(this).attr("id");
-        if ((sfc.includes(selected))){  
-            if (chosenChar === false){
-                statted = $(this).data('stats');
-                altThis = this;
-                gameFunk.chooseChar();
-            } else if ((chosenChar === true) && (chosenEnem === false)){
-                statted = $(this).data('stats');
-                altThis = this;
-               gameFunk.chooseEnem();
-            }
-        };
-
-        if ((chosenEnem === true) && (charHealth > 0)){
-            $("#atk-btn").css("display", "flex");
-        };
-    });
-
-    $("#atk-btn").click(function(){
-        if (chosenEnem === true){
-            
-            dmgGiv = Math.floor(Math.random() * charAttack) + attackAdd;
-            dmgRec = Math.floor(Math.random() * enemCounter);
-            charHealth -= dmgRec;
-            enemHealth -= dmgGiv;
-            gameFunk.battleLog();
-            $("#player-char").find(".health-show").text(charHealth);
-            $("#opponent").find(".health-show").text(enemHealth);
-            if (charHealth <= 0){
-                $("#player-char").find(".health-show").text(0);
-                $("#rst-btn").css("display", "flex");
-                $("#atk-btn").css("display", "none");
-                if (enemHealth > 0){
-                    $("#buttonInOut").prepend(newHead.text("You lost!"));
-                    newHead.attr("class", "col-12 text-center text-danger");
-                };
-            }; 
-            
-            if (enemHealth <= 0){
-                $("#opponent").find(".health-show").text(0);
-                $("#atk-btn").css("display", "none");
-                chosenEnem = undefined;
-                attackAdd += 12;
-                newHead.attr("class", "col-12 text-danger");
-                readyChar.append(newHead.text("Choose your enemy!"));
-                $("#opponent").children().css("display", "none");
-                if (pickedChar.length === 4){
-                    $("#buttonInOut").prepend(newHead.text("You Won!!!"));
-                    newHead.attr("class", "col-12 text-center text-success");
+    attackButton: function(){
+        $("#atk-btn").click(function(){
+            if (chosenEnem === true){
+                
+                dmgGiv = Math.floor(Math.random() * charAttack) + attackAdd;
+                dmgRec = Math.floor(Math.random() * enemCounter);
+                charHealth -= dmgRec;
+                enemHealth -= dmgGiv;
+                gameFunk.battleLog();
+                $("#player-char").find(".health-show").text(charHealth);
+                $("#opponent").find(".health-show").text(enemHealth);
+                if (charHealth <= 0){
+                    $("#player-char").find(".health-show").text(0);
                     $("#rst-btn").css("display", "flex");
-                }
+                    $("#atk-btn").css("display", "none");
+                    if (enemHealth > 0){
+                        $("#buttonInOut").prepend(newHead.text("You lost!"));
+                        newHead.attr("class", "col-12 text-center text-danger");
+                    };
+                }; 
+                
+                if (enemHealth <= 0){
+                    $("#opponent").find(".health-show").text(0);
+                    $("#atk-btn").css("display", "none");
+                    chosenEnem = undefined;
+                    attackAdd += 12;
+                    newHead.attr("class", "col-12 text-danger");
+                    readyChar.append(newHead.text("Choose your enemy!"));
+                    $("#opponent").children().css("display", "none");
+                    if (pickedChar.length === 4){
+                        $("#buttonInOut").prepend(newHead.text("You Won!!!"));
+                        newHead.attr("class", "col-12 text-center text-success");
+                        $("#rst-btn").css("display", "flex");
+                    }
+                };
+                
+                if ((charHealth < 0) && (enemHealth < 0)){
+                    $("#rst-btn").css("display", "flex");
+                    $("#buttonInOut").prepend(newHead.text("It's a tie!"));
+                    newHead.attr("class", "col-12 text-center text-warning");
+                };
+                
             };
-            
-            if ((charHealth < 0) && (enemHealth < 0)){
-                $("#rst-btn").css("display", "flex");
-                $("#buttonInOut").prepend(newHead.text("It's a tie!"));
-                newHead.attr("class", "col-12 text-center text-warning");
-            };
-            
-        };
-    });
+        });
+    },
 
-    $("#rst-btn").click(function() {
+    resetButton: function(){
+        $("#rst-btn").click(function() {
             charHealth = "";
             charAttack = "";
             enemStats = "";
@@ -153,20 +163,27 @@ $(document).ready(function(){
             $("#pictures-row").css("display", "flex");
             $("#rst-btn").css("display", "none");
             $("#fight-log").empty();
-    });
+        });
+    },
 
-    $("div").click(function(){
-        if(chosenEnem === undefined){
-            if ((pickedChar.length === 4) || (charHealth <= 0)){
-                return;
+    chooseAgainEnem: function(){
+        $("div").click(function(){
+            if(chosenEnem === undefined){
+                if ((pickedChar.length === 4) || (charHealth <= 0)){
+                    return;
+                };
+                $("#pictures-row").css("display", "flex");
+                selected = $(this).attr("id");
+                if ((sfc.includes(selected))){
+                    statted = $(this).data('stats');
+                    altThis = this;
+                    gameFunk.chooseEnem();
+                };
             };
-            $("#pictures-row").css("display", "flex");
-            selected = $(this).attr("id");
-            if ((sfc.includes(selected))){
-                statted = $(this).data('stats');
-                altThis = this;
-                gameFunk.chooseEnem();
-            };
-        };
-    });
+        });
+    }
+};
+
+$(document).ready(function(){
+    gameFunk.gameStart();
 });
